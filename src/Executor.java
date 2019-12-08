0e36782d95af02cdcf6e64e3bd90838e8ee4b063
Promethium14 <yasyulyanets@gmail.com>
2019-12-08 20:51:53 +0200

    task 6;

--- src/Executor.java
+++ src/Executor.java
@@ -1,15 +1,32 @@
 import model.Booking;
 
 import java.text.ParseException;
+import java.util.Date;
+import java.util.Random;
 
 import util.InfoGenerator;
+import util.Report;
+import util.ReportException;
 
 public class Executor {
 
     public static void main(String[] args) throws ParseException {
-        Booking booking = InfoGenerator.generateBooking();
-        booking.print();
-        System.out.println("-----------------------------");
-        booking.print(true);
+
+        Report report = new Report();
+        for (int i = 0; i < 10; i++) {
+            report.addBooking(InfoGenerator.generateBooking());
+        }
+        Date date = report.getLastBooking().getDate();
+        try {
+            report.find(date).print();
+        } catch (ReportException reportException) {
+            reportException.printStackTrace();
+        }
+        int number = new Random().nextInt(); //aaaaaaaaaaaaaaaaaaaaaaaa
+        try {
+            report.find(number).print();
+        } catch (ReportException reportException) {
+            reportException.printStackTrace();
+        }
     }
 }
--- src/util/ReportException.java
+++ src/util/ReportException.java
@@ -1,7 +1,7 @@
 package util;
 
 public class ReportException extends RuntimeException {
-    private static final String MESSAGE = "Unable to find element.";
+    private static final String MESSAGE = "Unable to find booking in report queue.";
 
     public ReportException() {
         super(MESSAGE);
